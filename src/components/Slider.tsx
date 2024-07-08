301748cbe070925064ec289c057bfcb824a78a38
Lasha <lashiko.deisadze@gmail.com>
2024-07-08 11:55:34 +0400

    temp finished slider

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -2,14 +2,14 @@
 
 import Image from "next/image";
 import Link from "next/link";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const slides = [
   {
     id: 1,
     title: "Summer Sale Collections",
     description: "Sale! Up to 50% off!",
-    img: "https://images.pexels.com/photos/11034412/pexels-photo-11034412.jpeg",
+    img: "https://images.pexels.com/photos/1247526/pexels-photo-1247526.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
     url: "/",
     bg: "bg-gradient-to-r from-red-50 to-pink-50",
   },
@@ -41,23 +41,40 @@ const slides = [
 
 const Slider = () => {
   const [current, setCurrent] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrent((curr) => (curr == slides.length - 1 ? 0 : curr + 1));
+    }, 5000);
+    return () => clearInterval(interval);
+  });
+
   return (
     <div className="h-[calc(100vh-80px)] overflow-hidden">
-      <div className="flex h-full w-max transition-all duration-1000 ease-in-out">
+      <div
+        className="flex h-full w-max transition-all duration-1000 ease-in-out"
+        style={{ transform: `translateX(-${current * 100}vw)` }}
+      >
         {slides.map((slide) => {
           return (
             <div
               className={`${slide.bg} flex h-full w-screen flex-col gap-16 xl:flex-row`}
               key={slide.id}
             >
-              <div className="w-1/2">
-                <h2>{slide.description}</h2>
-                <h1>{slide.title}</h1>
+              <div className="flex h-1/2 flex-col items-center justify-center gap-8 text-center xl:h-full xl:w-1/2 2xl:gap-12">
+                <h2 className="text-xl lg:text-3xl 2xl:text-5xl">
+                  {slide.description}
+                </h2>
+                <h1 className="text-5xl font-semibold lg:text-6xl 2xl:text-8xl">
+                  {slide.title}
+                </h1>
                 <Link href={slide.url}>
-                  <button>SHOP NOW</button>
+                  <button className="rounded-md bg-black px-4 py-3 text-white">
+                    SHOP NOW
+                  </button>
                 </Link>
               </div>
-              <div className="relative w-1/2">
+              <div className="relative h-1/2 xl:h-full xl:w-1/2">
                 <Image
                   src={slide.img}
                   alt={slide.title}
@@ -69,6 +86,19 @@ const Slider = () => {
             </div>
           );
         })}
+      </div>
+      <div className="absolute bottom-8 left-1/2 m-auto flex gap-4">
+        {slides.map((slide, index) => (
+          <div
+            className={`flex h-3 w-3 cursor-pointer items-center justify-center rounded-full ring-1 ring-gray-600 ${current === index ? "scale-150" : ""}`}
+            key={slide.title}
+            onClick={() => setCurrent(index)}
+          >
+            {current == index && (
+              <div className="h-[6px] w-[6px] rounded-full bg-gray-600"></div>
+            )}
+          </div>
+        ))}
       </div>
     </div>
   );
